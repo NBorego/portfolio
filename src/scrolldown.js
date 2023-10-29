941f1c9a06e2ea8d5247f179941f7621cf92c67e
Nicolas Borego <129863873+NBorego@users.noreply.github.com>
2023-10-29 18:35:43 -0300

    scrolldown
    animação para o scrolldown

---
+++ src/scrolldown.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+    const scrolldown = document.querySelector('.scrolldown');
+    const position = window.pageYOffset;
+    var opacity = 1 - position / 200;
+
+    if (opacity < 0) opacity = 0;
+    scrolldown.style.opacity = opacity;
+});
