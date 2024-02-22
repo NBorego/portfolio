bf3603fca3f09ce7faba7b3ff0370185c5303464
NBorego <n.s.borego@gmail.com>
2024-02-22 13:04:12 -0300

    Navigation update

--- src/nav.js
+++ src/nav.js
@@ -1,32 +1,32 @@
-var navLinks = document.querySelector('.nav-links');
-var iconBurger = document.querySelector('.icon-burger');
-var iconClose = document.querySelector('.icon-close');
+var $navLinks = document.querySelector(".nav-links");
+var $iconBurger = document.querySelector(".icon-burger");
+var $iconClose = document.querySelector(".icon-close");
 
-iconBurger.addEventListener('click', () => {
-    navLinks.style.display = 'flex';
+$iconBurger.addEventListener("click", () => {
+    $navLinks.style.display = "flex";
 
-    iconBurger.style.display = 'none';
-    iconClose.style.display = 'block';
+    $iconBurger.style.display = "none";
+    $iconClose.style.display = "block";
 });
 
-iconClose.addEventListener('click', () => {
-    navLinks.style.display = 'none';
+$iconClose.addEventListener("click", () => {
+    $navLinks.style.display = "none";
 
-    iconClose.style.display = 'none';
-    iconBurger.style.display = 'block';
+    $iconClose.style.display = "none";
+    $iconBurger.style.display = "block";
 });
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
     if (innerWidth >= 768) {
-        navLinks.style.display = 'flex';
-        iconBurger.style.display = 'none';
-        iconClose.style.display = 'none';
+        $navLinks.style.display = "flex";
+        $iconBurger.style.display = "none";
+        $iconClose.style.display = "none";
     } else {
-        if (iconClose.style.display === 'block') {
-            return
+        if ($iconClose.style.display === "block") {
+            return;
         } else {
-            iconBurger.style.display = 'block';
-            navLinks.style.display = 'none';
-        }    
+            $iconBurger.style.display = "block";
+            $navLinks.style.display = "none";
+        }
     }
 });
