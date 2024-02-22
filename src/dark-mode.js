3291482498b238715a2f939289767b8f5dd64495
NBorego <n.s.borego@gmail.com>
2024-02-22 12:37:06 -0300

    dark mode

--- src/dark-mode.js
+++ src/dark-mode.js
@@ -1,32 +1,17 @@
-const switchButton = document.querySelector('.switch-button');
-const links = document.querySelector('.link-navigation');
-const switchIconImage = document.querySelector('#switch-img');
-const body = document.querySelector('body');
-const root = document.documentElement;
+const $switchButton = document.querySelector(".switch-button");
+const $switchIconImage = document.querySelector("#switch-img");
+const $root = document.documentElement;
 
-var theme = 'light';
+$switchButton.addEventListener("click", () => {
+    if ($root.classList.contains("light")) {
+        $switchButton.style.justifyContent = "flex-end";
+        $switchIconImage.src = "./assets/moon.svg";
 
-switchButton.addEventListener('click', () => {
-    // if (switchButton.style.justifyContent === 'flex-start')
-    if (theme === 'light') {
-        theme = 'dark';
-
-        switchIconImage.src = '../assets/moon.svg';
-        switchButton.style.justifyContent = 'flex-end';
-
-        root.style.setProperty('--gray', '#1E293B');
-        root.style.setProperty('--very-gray', '#334155');
-        root.style.setProperty('--slate', '#fff');
-        root.style.setProperty('--white', '#0f172a');
+        $root.classList.replace("light", "dark");
     } else {
-        theme = 'light';
+        $switchIconImage.src = "./assets/sun.svg";
+        $switchButton.style.justifyContent = "flex-start";
 
-        switchIconImage.src = '../assets/sun.svg';
-        switchButton.style.justifyContent = 'flex-start';
-
-        root.style.setProperty('--very-gray', '#9ca3af');
-        root.style.setProperty('--gray', '#E5E7EB');
-        root.style.setProperty('--slate', '#0f172a');
-        root.style.setProperty('--white', '#fff');
+        $root.classList.replace("dark", "light");
     }
 });
